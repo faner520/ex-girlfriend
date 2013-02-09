f656bb0b6565716050b011447bbf06f6653944c6
Balakrishnan <balakrishnan@vangal.com>
2013-02-09 20:23:05 +0530

    added xml generation logic test 11

--- routes/services.js
+++ routes/services.js
@@ -9,6 +9,8 @@ var sendgrid = new SendGrid(user, key);
 var client = require('twilio')('ACe77e74197147dbef14db8a9c545a21ac', '1d2cb94a954c8111f1613dec4b726dec');
 var js2xmlparser = require("js2xmlparser");
 var url = require('url');
+var querystring = require("querystring");
+
 
 exports.getEmail = function(req, res) {
     console.log(req.body.text + "|FROM|" + req.body.subject +req.body.from+req.body.to);
