e29bdef69ed92a6bc17861144596769f81efcd73
Balakrishnan <balakrishnan@vangal.com>
2013-02-09 16:19:43 +0530

    Sendgrid logic

--- routes/services.js
+++ routes/services.js
@@ -4,17 +4,19 @@ var key = "kannan88";
 var sendgrid = new SendGrid(user, key);
 
 exports.getEmail = function(req, res) {
-    console.log(req.body.text + "|FROM|" + req.body.subject);
+    console.log(req.body.text + "|FROM|" + req.body.subject +req.body.from+req.body.to);
     res.statusCode = 200;
     res.send("Ok");
-    sendgrid.send({
-		  to: 'vk.balakrishnan@gmail.com',
-		  from: 'yourex@vkb.me',
-		  subject: 'Hello World',
-		  text: 'My first email through SendGrid'
-		}, function(success, message) {
-		  if (!success) {
-		    console.log(message);
-		  }
-		});
+    if(req.body.to == "ex@vkb.me"){
+	    sendgrid.send({
+			  to: req.body.from,
+			  from: 'yourex@vkb.me',
+			  subject: 'You will recieve a call shortly',
+			  text: 'Hey,\nI will call you as soon as possible. I know this is a template reply, but I will call.\nYour X.'
+			}, function(success, message) {
+			  if (!success) {
+			    console.log(message);
+			  }
+			});
+	  }
 };
