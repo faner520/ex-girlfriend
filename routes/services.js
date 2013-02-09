e92f20f15bb919357d11d57aad8e35ab5c07bd70
Balakrishnan <balakrishnan@vangal.com>
2013-02-09 17:06:07 +0530

    bug fix push 3

--- routes/services.js
+++ routes/services.js
@@ -8,11 +8,11 @@ exports.getEmail = function(req, res) {
     console.log(req.body.text + "|FROM|" + req.body.subject +req.body.from+req.body.to);
     res.statusCode = 200;
     res.send("Ok");
-    var valid = req.body.to..match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);;
+    var valid = req.body.to.match(/([a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z0-9._-]+)/gi);;
     console.log(valid);
     if( valid == "ex@vkb.me"){
 	    sendgrid.send({
-			  to: req.body.from,
+			  to: valid,
 			  from: 'yourex@vkb.me',
 			  subject: 'You will recieve a call shortly',
 			  text: 'Hey,\nI will call you as soon as possible. I know this is a template reply, but I will call.\nYour X.'
