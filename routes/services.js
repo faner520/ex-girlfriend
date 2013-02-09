0689834bfd46c4dbb0282730e3cad5c3779413f2
Balakrishnan <balakrishnan@vangal.com>
2013-02-09 17:55:07 +0530

    bug fix push 8 - anand's cred

--- routes/services.js
+++ routes/services.js
@@ -1,6 +1,10 @@
 var SendGrid = require('sendgrid').SendGrid;
+/*
 var user = "vk.balakrishnan";
 var key = "kannan88";
+*/
+var user = "anand";
+var key = "iceiceicecream";
 var sendgrid = new SendGrid(user, key);
 var client = require('twilio')('ACe77e74197147dbef14db8a9c545a21ac', '1d2cb94a954c8111f1613dec4b726dec');
 
