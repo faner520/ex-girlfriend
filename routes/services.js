cf6a69a8cd3e1d66129c4164e7cbe0a31f087f1b
Balakrishnan <balakrishnan@vangal.com>
2013-02-09 15:24:31 +0530

    sendgrid msg

--- routes/services.js
+++ routes/services.js
@@ -1,3 +1,13 @@
 exports.getEmail = function(req, res) {
-    console.log('got post');
+    console.log(req);
+    sendgrid.send({
+		  to: 'yourex@vkb.me',
+		  from: 'vk.balakrishnan@gmail.com',
+		  subject: 'Hello World',
+		  text: 'My first email through SendGrid'
+		}, function(success, message) {
+		  if (!success) {
+		    console.log(message);
+		  }
+		});
 };
